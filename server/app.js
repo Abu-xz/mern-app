f70b2191950b68e82ed6c95670f9bd24284cc6ed
Abbas firdous <abbasfathima40@gmail.com>
2025-03-18 21:58:50 +0530

    mongodb connected

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
 dotenv.config(); // configuration for env variables
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
+mongoose.connect(MONGO_URI)
+        .then(() => {
+            console.log('Connected to MongoDb');
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
 app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`)
