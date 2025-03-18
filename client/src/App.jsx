40f24dceca060e41fcba6e372db5ea80f6b6a3f2
Abbas firdous <abbasfathima40@gmail.com>
2025-03-18 20:17:16 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import React from 'react'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <div className='text-red-600'>
+            Signup
+        </div>
+    )
+}
+
+export default Signup
