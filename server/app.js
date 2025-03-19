9ffa87ca08a1ebaffa413e3483901f5b15e49bd7
Abbas firdous <abbasfathima40@gmail.com>
2025-03-19 15:15:58 +0530

    router and controller created and tested

--- server/app.js
+++ server/app.js
@@ -2,11 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
+import userRouter from './routes/userRoute.js';
+
 dotenv.config(); // configuration for env variables
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+
+app.use(express.json()) // middleware to parse json
+app.use('/api/users', userRouter)
+
+
+
 
 mongoose.connect(MONGO_URI)
         .then(() => {
