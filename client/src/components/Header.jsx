b5628dfd12f9a85c2cc28e85346454b04002c523
Abbas firdous <abbasfathima40@gmail.com>
2025-03-19 18:17:47 +0530

    sign up and login UI completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,9 +8,10 @@ const Header = () => {
                 <h1 className=' text-md font-bold md:text-2xl'>CRUD APP</h1>
             </Link>
             <nav className='flex'>
-                <ul className='flex justify-evenly gap-x-10'>
+                <ul className='flex justify-evenly gap-x-5 md:gap-x-10'>
                     <Link to={'/'} className='cursor-pointer hover:text-slate-400 px-2 rounded text-sm md:text-lg'>Home</Link>
                     <Link to={'/about'} className='cursor-pointer hover:text-slate-400 px-2 rounded text-sm md:text-lg'>About</Link>
+                    <Link to={'/login'} className='cursor-pointer hover:text-slate-400 px-2 rounded text-sm md:text-lg'>login</Link>
                     <Link to={'/profile'} className='cursor-pointer hover:text-slate-400 px-2 rounded text-sm md:text-lg'>Profile</Link>
                 </ul>
             </nav>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className='text-red-600'>
-    Login
-  </div>
+    <div className="flex flex-col justify-center items-center min-h-screen  p-4">
+            <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full">
+                <h1 className="text-3xl text-center font-bold uppercase">Log in</h1>
+                <form className="flex flex-col gap-4 mt-4">
+                    <input type="text" className="bg-slate-200 p-2 rounded" placeholder="Username" />
+                    <input type="email" className="bg-slate-200 p-2 rounded" placeholder="Email" />
+                    <input type="password" className="bg-slate-200 p-2 rounded" placeholder="Password" />
+                    <button className="bg-black text-white p-2 rounded hover:bg-gray-800 transition">Submit</button>
+                </form>
+                <p className='mt-4 text-end text-gray-500'> Don't have an account? <Link className='text-blue-500' to={'/signup'}>Sign Up</Link></p>
+            </div>
+        </div>
+
   )
 }
 
