a19a571257831159f35652506afc1d9aea6445fe
Abbas firdous <abbasfathima40@gmail.com>
2025-03-21 17:13:15 +0530

    created api for user update and delete

--- server/controllers/admin/adminController.js
+++ server/controllers/admin/adminController.js
@@ -1,4 +1,6 @@
+import { request } from "express"
 import User from "../../models/userModel.js"
+import { errorHandler } from "../../utils/error.js"
 
 
 export const fetchUsers = async (request, response) => {
@@ -13,5 +15,36 @@ export const fetchUsers = async (request, response) => {
     } catch (error) {
         console.log('user fetch error, ', error)
         response.status(500).json({ message: 'Server error' });
+    }
+}
+
+export const deleteUser = async (request, response, next) => {
+    try {
+
+        const {id} = request.params;
+        console.log('User id: ',id)
+        await User.findByIdAndDelete(id);
+        response.status(200).json({message: 'User deleted successfully'})
+
+    } catch (error) {
+        console.log(error);
+        next(errorHandler(500, 'Failed to delete user'))
+    }
+}
+
+export const updateUser = async (request, response, next) => {
+    try {
+        const {id} = req.params;
+        const {name, email, role} = req.body;
+
+        const updatedUser = await User.findByIdAndUpdate(id, {userName: name, email, role}, {new: true});
+        if(!updatedUser){
+            return response.status(404).json({success: false, message: 'User not found!'})
+        }
+
+        response.status(200).json(updatedUser);
+    } catch (error) {
+        console.log(error)
+        next(errorHandler(500, 'Failed to updated user.'))
     }
 }
--- server/routes/admin/authRoute.js
+++ server/routes/admin/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { login, logout } from '../../controllers/admin/authController.js';
-import { fetchUsers } from '../../controllers/admin/adminController.js';
+import { fetchUsers, deleteUser, updateUser } from '../../controllers/admin/adminController.js';
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post('/login', login);
 router.get('/logout', logout);
 
 router.get('/users', fetchUsers);
+router.put('/users/:id', updateUser)
+router.delete('/users/:id', deleteUser)
 
 export default router;
