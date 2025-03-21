dc3b74fba34eacf169f93472326709fd577fc967
Abbas firdous <abbasfathima40@gmail.com>
2025-03-21 22:46:42 +0530

    created api for admin to create user

--- server/controllers/admin/adminController.js
+++ server/controllers/admin/adminController.js
@@ -1,28 +1,46 @@
-import { request } from "express"
 import User from "../../models/userModel.js"
 import { errorHandler } from "../../utils/error.js"
+import bcryptjs from 'bcryptjs'
 
 
 export const fetchUsers = async (request, response) => {
     try {
-        const users = await User.find({role: {$ne: 'admin'}}) // Only users will retrieved
-        if(!users){
+        const users = await User.find({ role: { $ne: 'admin' } }) // Only users will retrieved
+        if (!users) {
             console.log('No users found!')
-            return response.status(404).json({success: true, message: 'No users found'})
+            return response.status(404).json({ success: true, message: 'No users found' })
         }
 
-        response.status(200).json({success: true, users});
+        response.status(200).json({ success: true, users });
     } catch (error) {
         console.log('user fetch error, ', error)
         response.status(500).json({ message: 'Server error' });
     }
 }
 
+export const updateUser = async (request, response, next) => {
+    try {
+        console.log('update user route reached')
+        const { id } = request.params;
+        const { userName, email, role } = request.body;
+
+        const updatedUser = await User.findByIdAndUpdate(id, { userName, email, role }, { new: true });
+        if (!updatedUser) {
+            return response.status(404).json({ success: false, message: 'User not found!' })
+        }
+
+        response.status(200).json(updatedUser);
+    } catch (error) {
+        console.log(error)
+        next(errorHandler(500, 'Failed to updated user.'))
+    }
+}
+
 export const deleteUser = async (request, response, next) => {
     try {
-        const {id} = request.params;
+        const { id } = request.params;
         await User.findByIdAndDelete(id);
-        response.status(200).json({message: 'User deleted successfully'})
+        response.status(200).json({ message: 'User deleted successfully' })
 
     } catch (error) {
         console.log(error);
@@ -30,20 +48,37 @@ export const deleteUser = async (request, response, next) => {
     }
 }
 
-export const updateUser = async (request, response, next) => {
+export const createUser = async (req, res, next) => {
     try {
-        console.log('update user route reached')
-        const {id} = request.params;
-        const {userName, email, role} = request.body;
+        console.log('create user route reached');
+        const { userName, email, password, role } = req.body;
 
-        const updatedUser = await User.findByIdAndUpdate(id, {userName, email, role}, {new: true});
-        if(!updatedUser){
-            return response.status(404).json({success: false, message: 'User not found!'})
+        if (!userName || !password || !email) {
+            console.log("Missing fields");
+            return res.status(400).json({ success: false, message: "All fields are required" });
         }
 
-        response.status(200).json(updatedUser);
+        console.log("Checking if user exists...");
+        const userExist = await User.findOne({ email });
+
+        if (userExist) {
+            console.log("User already exists");
+            return res.status(400).json({ success: false, message: "User already exists..." });
+        }
+
+        console.log("Hashing password...");
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+
+        console.log("Creating new user...");
+        const newUser = new User({ userName, email, password: hashedPassword });
+
+        await newUser.save();
+        console.log("User created successfully");
+
+        return res.status(201).json({ success: true, message: "User created successfully" });
+
+
     } catch (error) {
-        console.log(error)
-        next(errorHandler(500, 'Failed to updated user.'))
+
     }
 }
--- server/routes/admin/authRoute.js
+++ server/routes/admin/authRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { login, logout } from '../../controllers/admin/authController.js';
-import { fetchUsers, deleteUser, updateUser } from '../../controllers/admin/adminController.js';
+import { fetchUsers, deleteUser, updateUser, createUser } from '../../controllers/admin/adminController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.get('/logout', logout);
 
+router.post('/create-user', createUser)
 router.get('/users', fetchUsers);
 router.put('/users/:id', updateUser)
 router.delete('/users/:id', deleteUser)
